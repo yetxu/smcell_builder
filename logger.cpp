a75a326dc8dc54b1d1be70f17788f9d1792518d4
yetxu <lusham034@gmail.com>
2025-06-28 12:49:51 +0800

    修改日志实现

--- logger.cpp
+++ logger.cpp
@@ -1,8 +1,12 @@
 #include"logger.h"
 #include"utils.h"
+#include <stdarg.h>
 using namespace std;
 
 #define LOG_TO_CONSOLE
+
+// 全局日志实例
+Logger g_logger;
 
 Logger::Logger(){
 
@@ -39,5 +43,40 @@ void Logger::LogToFile(const char* msg){
     cout << timestr;
     cout << msg << endl << endl;
 #endif
+}
 
+void Logger::LogToFile(LogLevel level, const char* format, ...) {
+    char buffer[1024];
+    va_list args;
+    va_start(args, format);
+    vsnprintf(buffer, sizeof(buffer), format, args);
+    va_end(args);
+    
+    char timestr[30];
+    GetTime(timestr);
+    
+    const char* levelStr = GetLogLevelStr(level);
+    
+    fout_ << timestr << "[" << levelStr << "] " << buffer << endl << endl;
+
+#ifdef LOG_TO_CONSOLE
+    cout << timestr << "[" << levelStr << "] " << buffer << endl << endl;
+#endif
+}
+
+const char* Logger::GetLogLevelStr(LogLevel level) {
+    switch (level) {
+        case LOG_LEVEL_DEBUG:
+            return "DEBUG";
+        case LOG_LEVEL_INFO:
+            return "INFO";
+        case LOG_LEVEL_WARN:
+            return "WARN";
+        case LOG_LEVEL_ERROR:
+            return "ERROR";
+        case LOG_LEVEL_FATAL:
+            return "FATAL";
+        default:
+            return "UNKNOWN";
+    }
 }
--- logger.h
+++ logger.h
@@ -1,6 +1,19 @@
 #ifndef LOGGER_H_
 #define LOGGER_H_
-#include<fstream>
+#include <fstream>
+#include <iostream>
+#include <cstdarg>
+#include <cstdio>
+#include <string>
+
+// 日志级别枚举
+enum LogLevel {
+    LOG_LEVEL_DEBUG,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_FATAL
+};
 class Logger{
 public:
     Logger();
@@ -8,8 +21,30 @@ public:
 
     void LogToFile(char* msg);
     void LogToFile(const char* msg);
+    void LogToFile(LogLevel level, const char* format, ...);
     void Init(const char* logfile);
 private:
+    const char* GetLogLevelStr(LogLevel level);
     std::ofstream fout_;
 };  
+
+// 全局日志实例
+extern Logger g_logger;
+
+// 日志宏定义
+#define LOG_INFO(format, ...) \
+    g_logger.LogToFile(LOG_LEVEL_INFO, format, ##__VA_ARGS__)
+
+#define LOG_ERROR(format, ...) \
+    g_logger.LogToFile(LOG_LEVEL_ERROR, format, ##__VA_ARGS__)
+
+#define LOG_DEBUG(format, ...) \
+    g_logger.LogToFile(LOG_LEVEL_DEBUG, format, ##__VA_ARGS__)
+
+#define LOG_WARN(format, ...) \
+    g_logger.LogToFile(LOG_LEVEL_WARN, format, ##__VA_ARGS__)
+
+#define LOG_FATAL(format, ...) \
+    g_logger.LogToFile(LOG_LEVEL_FATAL, format, ##__VA_ARGS__)
+
 #endif
